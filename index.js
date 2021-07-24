5ac86ecbd57465cb422f1489ad4fea3e1074fff7
itay_sc <itays@realcommerce.co.il>
2021-07-24 23:57:35 +0300

    add export defuault to components

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './src/components/Button';
+export { default as Chip } from './src/components/Chip';
+export { default as ChipsInput } from './src/components/ChipsInput';
+export { default as Colors } from './src/components/Colors';
