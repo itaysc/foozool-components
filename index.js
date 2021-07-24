24ac6dd01cdb0e27c69099b25bad47b8a85c36b5
itay_sc <itays@realcommerce.co.il>
2021-07-25 00:12:54 +0300

    fix exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { default as Button } from './src/components/Button';
-export { default as Chip } from './src/components/Chip';
-export { default as ChipsInput } from './src/components/ChipsInput';
-export { default as Colors } from './src/components/Colors';
+export { default as Button } from './src/components/Button/Button';
+export { default as Chip } from './src/components/Chip/Chip';
+export { default as ChipsInput } from './src/components/ChipsInput/ChipsInput';
+export { default as Colors } from './src/components/Colors/Color';
